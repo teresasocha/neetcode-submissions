15ea22969c0e65b6cad3138f76ed7761c6a5163b
teresasocha <teresasocha27@gmail.com>
2026-04-25 20:50:20 +0200

    Add: maximum-number-of-balloons - submission-0

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        b = text.count('b')
+        a = text.count('a')
+        l = int(text.count('l')/2)
+        o = int(text.count('o')/2)
+        n = text.count('n')
+        balloon = [b, a, l, o, n]
+        return min(balloon)
